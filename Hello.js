8d2060ccffe8334b68075cb40307bb389b99d025
elbroandrew <elbroandrew@gmail.com>
2022-06-08 22:29:58 +1000

    put components into separate files

---
+++ Hello.js
@@ -0,0 +1,13 @@
+
+class Hello extends React.Component {
+    render() {
+        const msg = <h2>World!</h2>;
+        return (
+            <div>
+                <p> Hello </p>
+                { msg }
+            </div>
+        );
+    }
+}
+
--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-function getNum() {
-    return Math.floor(Math.random() * 10) + 1;
-}
-
-class NumPicker extends React.Component {
+class App extends React.Component {
     render() {
-        const num = getNum();
         return (
             <div>
-                <h1>My number is: {num}</h1>
-                <p> {num === 7 ? "Congrats!" : "Sad"}</p>
-                {num === 5 && <h1>The number "five"</h1>} {/* если в && первое true, то второе выполнится. */}
+                <Hello />
             </div>
         );
     }
 }
 
-ReactDOM.render(<NumPicker />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
