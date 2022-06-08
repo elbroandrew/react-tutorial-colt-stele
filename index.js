d437f3a5a4d71f07901ebc81f544da6638add361
elbroandrew <elbroandrew@gmail.com>
2022-06-08 22:07:45 +1000

    embedding JS into JSX

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ class JSXDemo extends React.Component {
     render() {
         return (
             <div>
-                <h1>My image</h1>
-                <img src="https://images.unsplash.com/photo-1652847042924-0615a274d629?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+                <h1>My number is: { 2 * 8.5 }</h1>
             </div>
         );
     }
