7cef74058d37cb0ddbdccf0af9e857379de6eb0c
elbroandrew <elbroandrew@gmail.com>
2022-06-08 22:05:07 +1000

    JSX class but for v17

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-// class Hello extends React.Component{
-//     render(){
-//         return (
-//         <div>
-//         <h1>Hello there!</h1>
-//         <h1>Hello there!</h1>
-//         <h1>Hello there!</h1>
-//         </div>
-//         );
-//     }
-// }
-
-function Hello(){
-    return (
-        <div>
-            <h1>Hello there!</h1>
-            <h1>Hello there!</h1>
-            <h1>Hello there!</h1>
-        </div>
-    );
+class JSXDemo extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>My image</h1>
+                <img src="https://images.unsplash.com/photo-1652847042924-0615a274d629?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+            </div>
+        );
+    }
 }
 
-ReactDOM.render(<Hello/>, document.getElementById('root'));
+ReactDOM.render(<JSXDemo />, document.getElementById("root"));
