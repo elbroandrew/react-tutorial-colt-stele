bf2cb2c7cb8ab80eedcac4c39714566c9e3cb722
elbroandrew <elbroandrew@gmail.com>
2022-06-08 22:09:58 +1000

    embed a function

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+function getMood() {
+    const moods = ["Angry", "Happy", "Silly"];
+    return moods[Math.floor(Math.random() * moods.length)];
+}
+
 class JSXDemo extends React.Component {
     render() {
         return (
             <div>
-                <h1>My number is: { 2 * 8.5 }</h1>
+                <h1>My mood is: {getMood()}</h1>
             </div>
         );
     }
