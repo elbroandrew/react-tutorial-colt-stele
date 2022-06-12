30236f232345f09a5955e95cdbfe28d6cae4fe55
elbroandrew <elbroandrew@gmail.com>
2022-06-13 02:51:32 +1000

    styles 2 inline and conditions

--- Machine.js
+++ Machine.js
@@ -1,11 +1,17 @@
 
 class Machine extends React.Component {
     render() {
-        const {s1, s2, s3} = this.props;
+        const { s1, s2, s3 } = this.props;
+        const colors = { fontSize: '50px', backgroundColor: 'purple' };
         return (
             <div className="Machine">
-                <p style={{fontSize: '50px', backgroundColor: 'purple'}}>{s1}{s2}{s3}</p>
-                <p>{s1 === s2 && s3 ? "You win!" : "You lose!"}</p>
+
+                <p style={colors}>{s1}{s2}{s3}</p>
+
+                <p className={s1 === s2 && s3 ? 'win' : 'lose'}>
+                    {s1 === s2 && s3 ? "You win!" : "You lose!"}
+                </p>
+
             </div>
         );
     }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ class App extends React.Component {
             <div>
                 <h1>Slot Machine!</h1>
                 <Machine s1="🍈" s2="🍌" s3="🍒"/>
+                <Machine s1="🍌" s2="🍌" s3="🍌"/>
             </div>
             
         );
